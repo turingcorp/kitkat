f06d90a532ffa722e63d4ffc4cc49fcdf582db7f
vauxhall <zero.griffin@gmail.com>
2016-09-02 19:58:22 -0500

    view racket

--- kitkat/View/VGameRacket.swift
+++ kitkat/View/VGameRacket.swift
@@ -2,5 +2,17 @@ import UIKit
 
 class VGameRacket:UIView
 {
+    weak var controller:CGame!
+    private let kCornerRadius:CGFloat = 4
     
+    convenience init(controller:CGame)
+    {
+        self.init()
+        userInteractionEnabled = false
+        backgroundColor = UIColor.blueColor()
+        clipsToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = kCornerRadius
+        self.controller = controller
+    }
 }
